c5a1415146d96f839ac125a66800ecbfc5020a7d
Sew-a <sevavetisyan97@gmail.com>
2021-01-08 15:15:57 +0400

    send requwsts

--- js/poor.js
+++ js/poor.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
       emailjs.init("user_IUFyW3twYDD5jnfPHSSlw");
     })();
 
-
     function selector(target){
       return document.querySelector(target).value;
     }
@@ -25,19 +24,36 @@ $(document).ready(function () {
 
     const message = selector("#message");
 
-    const contactInformation = `name - ${username} ${userlname}, mail ${userMail}, phone number ${phoneNumber}`;
-    const deliveryInformation = `adress - ${address1} ${address2}, city - ${city}, state - ${state}, zip - ${zip}, country ${country}`;
+    const contactInformation = ` ${username} ${userlname}`;
+    const addresses = `${address1} , ${address2}`;
+    const citys = `${city} , ${state}`;
+    const countrys = `${zip} , ${country}`;
+
+    const deliveryInformation = `${address1} , ${address2}`
 
     console.log(username);
     console.log(contactInformation);
     
   
 let templateParams = {
-    from_name: username,
+    from_name: contactInformation,
     mail: userMail,
+    phone: phoneNumber,
     to_name: "Jane",
-    contact_info: contactInformation,
-    delivery_info: deliveryInformation,
+    
+    address: addresses,
+    city: citys,
+    zip: countrys,
+
+    //  diamond cuts
+    diamondColor: "colorless",
+    caratSizes: 2.0,
+    cutShapes: "Pearl Cut",
+    certificates: "GIA",
+    jewelryID: 8852-7854-77,
+    ringSize: 5.25,
+    metalColor: "Rose gold",
+    ringEngraving: "Love You",
 
     message: message,
   }
