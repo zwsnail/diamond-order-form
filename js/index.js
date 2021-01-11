c9ba4be118e0f9302c873307037801087d21bfb4
Sew-a <sevavetisyan97@gmail.com>
2021-01-11 11:50:16 +0400

    completely end a form request

--- js/index.js
+++ js/index.js
@@ -8,48 +8,18 @@ $(document).ready(function () {
     let currentTab = $(this).attr("href");
     $(".sections").hide();
     $(currentTab).show();
+     
+    console.log(currentTab);
 
-    // return false;
+    if(currentTab === "#contact_section"){
+      $(".footer_space").hide();
+    } else {
+      $(".footer_space").show();
+    }
+    return false;
   });
+
+
+
 });
 
-// diamond boxes
-
-const obj = {
-  diamondColor: "",
-  caratSize: "",
-  cutShape: "",
-  certificates: "",
-  ringSize: "",
-  metalColor: "",
-};
-
-$("#diamond_box img").on("click", function () {
-  obj.diamondColor = $(this).attr("alt");
-  console.log(obj);
-});
-
-$(".cut_shape img").on("click", function () {
-  obj.cutShape = $(this).attr("alt");
-  console.log(obj);
-});
-
-$(".certificates img").on("click", function () {
-  obj.certificates = $(this).attr("alt");
-  console.log(obj);
-});
-
-$(".metals img").on("click", function () {
-  obj.metalColor = $(this).attr("alt");
-  console.log(obj);
-});
-
-$(".caratSize div").on("click", function () {
-  obj.caratSize = $(this).text();
-  console.log(obj);
-});
-
-$(".ring_sizes div").on("click", function () {
-  obj.ringSize = $(this).text();
-  console.log(obj);
-});
--- js/poor.js
+++ js/poor.js
@@ -7,6 +7,67 @@ $(document).ready(function () {
     return document.querySelector(target).value;
   }
 
+  $(".ring_engrave_form").on("submit", function(e){
+    e.preventDefault();
+    obj.jevelryId = selector("#style_id");
+    obj.ringEngrave = selector("#diamond_text_engrave");
+
+    $(".all-info p:nth-child(5) span").text(`${obj.jevelryId}`);
+    $(".all-info p:nth-child(8) span").text(`${obj.ringEngrave}`);
+  });
+
+  $(".personal_text").on("submit", function(e){
+    e.preventDefault();
+    obj.laserMark = selector("#laser_mark");
+  });
+
+
+  const obj = {
+    laserMark:"",
+    diamondColor: "",
+    caratSize: "",
+    cutShape: "",
+    certificates: "",
+    jevelryId:"",
+    ringSize: "",
+    metalColor: "",
+    ringEngrave: "",
+  };
+  
+  
+  $("#diamond_box img").on("click", function () {
+    obj.diamondColor = $(this).attr("alt");
+    $(".all-info p:nth-child(1) span").text(`${obj.diamondColor}`);
+  });
+  
+  $(".cut_shape img").on("click", function () {
+    obj.cutShape = $(this).attr("alt");
+    $(".all-info p:nth-child(3) span").text(`${obj.cutShape}`);
+  });
+  
+  $(".certificates img").on("click", function () {
+    obj.certificates = $(this).attr("alt");
+    $(".all-info p:nth-child(4) span").text(`${obj.certificates}`);
+  });
+  
+  $(".metals img").on("click", function () {
+    obj.metalColor = $(this).attr("alt");
+    $(".all-info p:nth-child(7) span").text(`${obj.metalColor}`);
+  });
+  
+  $(".caratSize div").on("click", function () {
+    obj.caratSize = $(this).text();
+    $(".all-info p:nth-child(2) span").text(`${obj.caratSize}`);
+  });
+  
+  $(".ring_sizes div").on("click", function () {
+    obj.ringSize = $(this).text();
+    $(".all-info p:nth-child(6) span").text(`${obj.ringSize}`);
+  });
+
+
+
+
   $("#contact_section").on("submit", function (e) {
     e.preventDefault();
 
@@ -31,8 +92,6 @@ $(document).ready(function () {
 
     const deliveryInformation = `${address1} , ${address2}`;
 
-    console.log(username);
-    console.log(contactInformation);
 
     let templateParams = {
       from_name: contactInformation,
@@ -45,14 +104,16 @@ $(document).ready(function () {
       zip: countrys,
 
       //  diamond cuts
-      diamondColor: "colorless",
-      caratSizes: 2.0,
-      cutShapes: "Pearl Cut",
-      certificates: "GIA",
-      jewelryID: 8852 - 7854 - 77,
-      ringSize: 5.25,
-      metalColor: "Rose gold",
-      ringEngraving: "Love You",
+      laserMark: obj.laserMark,
+
+      diamondColor: obj.diamondColor,
+      caratSizes: obj.caratSize,
+      cutShapes: obj.cutShape,
+      certificates: obj.certificates,
+      jewelryID: obj.jevelryId,
+      ringSize: obj.ringSize,
+      metalColor: obj.metalColor,
+      ringEngraving: obj.ringEngrave,
 
       message: message,
     };
